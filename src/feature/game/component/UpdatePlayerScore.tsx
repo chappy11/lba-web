914c31a588361ec0dfa32d9ce1cd9eb6be8d36ec
chappy <lumangyao112@gmail.com>
2025-06-15 19:09:58 +0800

    Fix 3 points

--- src/feature/game/component/UpdatePlayerScore.tsx
+++ src/feature/game/component/UpdatePlayerScore.tsx
@@ -94,7 +94,7 @@ export default function UpdatePlayerScore(
 							currentRebound
 						),
 						threepoints: parseInt(
-							currentPoints
+							currentThreePoints
 						),
 						assist: parseInt(
 							currentAssist
